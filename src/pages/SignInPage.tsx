4f6ab0b1f3a0afbb2a217a25c01e2edcb37d691d
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-03-25 01:42:11 +0700

    feat: Connected to api for sign in

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,18 +1,76 @@
 import type { FC } from 'react'
+import type { Rule } from 'antd/lib/form'
 
-import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { Button, Form, Input } from 'antd'
+
+import {
+  EnumAuthType,
+  useLoginEmailMutation,
+} from 'graphQL/generated/operations'
+
+import { path } from 'setup/PageRouter'
+
+import { baseFormProps } from 'helper/antdUtils'
 
 const SignInPage: FC = () => {
-  const navigate = useNavigate()
+  const [loginEmail, loginEmailResp] = useLoginEmailMutation({
+    onCompleted(resp) {
+      console.log(resp.loginEmail.payload)
+    },
+  })
+
+  const ruleRequired: Rule = {
+    required: true,
+    message: 'Required',
+  }
 
   return (
-    <div>
+    <SignInPageContainer>
       <h1>Sign In Page</h1>
 
-      <Button onClick={() => navigate('/profile')}>To profile page</Button>
-    </div>
+      <Form
+        {...baseFormProps}
+        onFinish={(values) => {
+          const { email, password } = values
+
+          loginEmail({
+            variables: {
+              authType: EnumAuthType.AuthToken,
+              email,
+              password,
+            },
+          })
+        }}
+      >
+        <Form.Item name="email" label="Email" rules={[ruleRequired]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item name="password" label="Password" rules={[ruleRequired]}>
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Link to={path.register}>Have no account? Create it.</Link>
+        </Form.Item>
+
+        <Button
+          block
+          type="primary"
+          htmlType="submit"
+          loading={loginEmailResp.loading}
+        >
+          Sign In
+        </Button>
+      </Form>
+    </SignInPageContainer>
   )
 }
 
 export default SignInPage
+
+const SignInPageContainer = styled.div`
+  text-align: center;
+`
