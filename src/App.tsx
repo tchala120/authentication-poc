003e48b69e353980304d09bea66aafca1778fd09
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-03-25 00:52:55 +0700

    fix: Used page router instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react'
 
+import './global.css'
+
+import PageRouter from 'setup/PageRouter'
+
 const App: FC = () => {
-  return (
-    <div className="App">
-      <span>Container</span>
-    </div>
-  )
+  return <PageRouter />
 }
 
 export default App
