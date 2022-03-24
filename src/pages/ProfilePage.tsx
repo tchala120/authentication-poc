d3ba5ee856a17adb1500818322442319323c8549
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-03-25 02:11:46 +0700

    feat: Add button to go to setting page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,11 @@
 import type { FC } from 'react'
 
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
+
 import useAuthContext from 'contexts/useAuthContext'
+
+import { path } from 'setup/PageRouter'
 
 const ProfilePage: FC = () => {
   const { user } = useAuthContext()
@@ -10,6 +15,10 @@ const ProfilePage: FC = () => {
       <h1>Profile Page</h1>
 
       <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <Link to={path.setting}>
+        <Button type="primary">Go to setting page</Button>
+      </Link>
     </div>
   )
 }
