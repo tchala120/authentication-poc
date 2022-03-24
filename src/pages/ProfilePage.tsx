c6078dfa9e05143bf2e2d6e3f15d37050fe2d1e6
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-03-25 02:10:34 +0700

    feat: Add user data on profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,17 @@
 import type { FC } from 'react'
 
+import useAuthContext from 'contexts/useAuthContext'
+
 const ProfilePage: FC = () => {
-  return <>Profile Page</>
+  const { user } = useAuthContext()
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  )
 }
 
 export default ProfilePage
